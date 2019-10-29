d9ca9d7695d1f09b2facc88b6081c5078a1c3be7
Chase Laptop <james.chase.richards@gmail.com>
2019-10-29 13:07:52 -0400

    all nodes command working

--- liri.js
+++ liri.js
@@ -1,34 +1,20 @@
 //code to read and set any environment variables with the dotenv package
 require("dotenv").config();
+const fs = require("fs");
 var axios = require("axios");
 var Spotify = require("node-spotify-api");
-var omdb = require("omdb");
-var bitKey = keys.bit;
-var bandsintown = require("bandsintown")(bitKey);
 
 //required to import the keys.js file and store it in a variable.
 var keys = require("./keys.js");
 
 //code to access key information
-var spotify = new Spotify(keys.spotify); //example Spotify url: http://open.spotify.com/track/b5e26f768da9407391038860d36d4a03
-var omdbKey = keys.omdb; //example OMDB url: http://www.omdbapi.com/?i=tt3896198&apikey=b3c1ae5c //example Bands In Town url: https://rest.bandsintown.com/artists/celine+dion/events?app_id=8396314a71d3546f2e1279216e727634
-
-// console.log(spotify);
-// console.log(omdbKey);
-// console.log(bitKey);
-
-//                  ********* GOAL **********
-// Make it so liri.js can take in one of the following commands:
-
-// concert-this         example: node liri.js concert-this '<band name here>'
-// spotify-this-song    example: node liri.js spotify-this-song '<song name here>'
-// movie-this           example: node liri.js movie-this '<movie name here>'
-// do-what-it-says      example: node liri.js do-what-it-says
-
-// var nodeInputs = process.argv.slice(2).join(" ");
+var spotify = new Spotify(keys.spotify);
+var omdbKey = keys.omdb;
+var bitKey = keys.bit;
 var command = process.argv[2];
 var searchTerm = process.argv.slice(3).join(" ");
 
+// concert-this         example: node liri.js concert-this '<band name here>'
 switch (command) {
   case "concert-this":
     axios
@@ -39,11 +25,11 @@ switch (command) {
           bitKey
       )
       .then(function(response) {
-        // If the axios was successful...
-        // Then log the body from the site!
         console.log(response.data[0]);
       });
     break;
+
+  // spotify-this-song    example: node liri.js spotify-this-song '<song name here>'
   case "spotify-this-song":
     spotify.search({ type: "track", query: searchTerm, limit: 1 }, function(
       err,
@@ -56,39 +42,29 @@ switch (command) {
     });
     break;
 
+  // movie-this           example: node liri.js movie-this '<movie name here>'
   case "movie-this":
-    // omdb.search("saw", function(err, movies) {
-    //   if (err) {
-    //     return console.error(err);
-    //   }
-
-    //   if (movies.length < 1) {
-    //     return console.log("No movies were found!");
-    //   }
-
-    //   movies.forEach(function(movie) {
-    //     console.log("%s (%d)", movie.title, movie.year);
-    //   });
-    // });
     axios
-      .get("http://www.omdbapi.com/?i=tt3896198&apikey=" + bitKey)
+      .get("http://www.omdbapi.com/?i=tt3896198&apikey=" + omdbKey.id)
       .then(function(response) {
-        // If the axios was successful...
-        // Then log the body from the site!
         console.log(response);
       });
     break;
+
+  // do-what-it-says      example: node liri.js do-what-it-says
   case "do-what-it-says":
+    spotify.search(
+      {
+        type: "track",
+        query: fs.readFileSync("./random.txt", { encoding: "utf8" }),
+        limit: 1
+      },
+      function(err, data) {
+        if (err) {
+          return console.log("Error occurred: " + err);
+        }
+        console.log(data.tracks.items[0]);
+      }
+    );
     break;
 }
-
-// spotify.search({ type: "track", query: "All the Small Things" }, function(
-//   err,
-//   data
-// ) {
-//   if (err) {
-//     return console.log("Error occurred: " + err);
-//   }
-
-//   console.log(data);
-// });
